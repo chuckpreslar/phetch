79eebc48ed1a4bb1b9bd8bb05f50c38807330300
Chuck Preslar <chuckpreslar@gmail.com>
2016-11-23 11:10:39 -0700

    Use async await.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var babel = require('gulp-babel');
 
 gulp.task('compile', function() {
   return gulp.src('lib/**/*.js')
-    .pipe(babel({ presets: ['es2015', 'stage-0'], plugins: ['transform-async-to-generator', 'transform-decorators-legacy'] }))
+    .pipe(babel({ presets: ['es2015', 'stage-0', 'stage-3'], plugins: ['transform-decorators-legacy'] }))
     .pipe(gulp.dest('dist'));
 });
